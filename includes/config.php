36ed14b273edf9cf31b5625d8f1cb46e9d95a772
L.Enrique Daza <110496417+Daza-Wody@users.noreply.github.com>
2025-06-18 10:37:31 -0500

    Update config.php
    Cambiar credenciales

--- includes/config.php
+++ includes/config.php
@@ -7,8 +7,8 @@
 
 
 // PRODUCCIÓN (HOSTING)
-define('DB_HOST', 'localhost');
-define('DB_USER', 'u148187998_seminario');
-define('DB_PASS', 'xviAniversario2025'); // La misma que usas para entrar
-define('DB_NAME', 'u148187998_db_registro');
+define('DB_HOST', '#############');
+define('DB_USER', '#############');
+define('DB_PASS', '#############'); // La misma que usas para entrar
+define('DB_NAME', '#############');
 ?>
